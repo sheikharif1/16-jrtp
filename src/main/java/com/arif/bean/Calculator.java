415972be63c4b4f6da47bf861c1c893ef8115c52
unknown <sheikh.arif111@gmail.com>
2021-07-20 13:19:22 +0530

    for jrtp practice first commit

---
+++ src/main/java/com/arif/bean/Calculator.java
@@ -0,0 +1,12 @@
+package com.arif.bean;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int i=10;
+int j=20;
+System.out.println(i+j);
+	}
+
+}
---
+++ src/main/java/com/arif/maven/MavenAppApplication.java
@@ -0,0 +1,13 @@
+package com.arif.maven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavenAppApplication.class, args);
+	}
+
+}
